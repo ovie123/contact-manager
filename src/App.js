afc16818be06e28e6f328214073d00ff6139b396
ovie123 <ernyovie@gmail.com>
2020-09-07 09:00:16 +0100

    added state and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Contacts from './components/contacts.component';
+import Header from './components/header.component.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header branding='Contact Manager' />
+      <div className='container'>
+        <Contacts />
+      </div>
     </div>
   );
 }
